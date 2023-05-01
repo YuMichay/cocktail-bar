069eb8361724b0c774a6c907e21d27961423bc6c
yuliya <julia.one.such@gmail.com>
2023-05-01 17:13:51 +0300

    fix: fix rules

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,6 +1,6 @@
 export const pageText = [
 `FEATURES
-\nEach girl in the game offers a special feature and may trigger it when the winning symbol's color matches the girl's color or you got 7+ girls of the same type.
+\nEach girl in the game offers a special feature and may trigger it when you get 7+ girls of the same type.
 \nPink hair girl - FREE SPIN!\nBrunette - DOUBLE MULLED WINE SYMBOL's COST NEXT WIN!\nBlonde - DOUBLE WIN!
 \nGIRLS
 \nPink hair girl - 1.20 ¤\nBrunette girl - 1.20 ¤\nBlonde girl - 1.20 ¤`,
@@ -14,11 +14,11 @@ export const pageText = [
 \n7 or more symbols or girls of the same type pay anywhere on the screen. The value of the win is determined by the total number of the same symbols shown on the screen at the end of a spin.
 \n7+ symbols: symbol's amount * symbol's cost + a half of your stake\n14+ symbols: symbol's amount * double symbol's cost + a half of your stake
 \n7+ girls: girl's amount * girls's cost + a half of your stake\n14+ girls: girl's amount * double girl's cost + a half of your stake
-\nMinimal stake: ¤ 0.20\nMaximal stake: ¤ 10`];
+\nMinimum stake: ¤ 0.20\nMaximum stake: ¤ 10`];
 
 export const smallPageText = [
   `FEATURES
-  \nEach girl in the game offers a special feature and may trigger it when the winning symbol's color matches the girl's color or you got 7+ girls of the same type.
+  \nEach girl in the game offers a special feature and may trigger it when you get 7+ girls of the same type.
   \nPink hair girl - FREE SPIN!\nBrunette - DOUBLE MULLED WINE SYMBOL's COST NEXT WIN!\nBlonde - DOUBLE WIN!`,
   `\nGIRLS
   \nPink hair girl - 1.20 ¤\nBrunette girl - 1.20 ¤\nBlonde girl - 1.20 ¤
@@ -32,4 +32,4 @@ export const smallPageText = [
   \n7 or more symbols or girls of the same type pay anywhere on the screen. The value of the win is determined by the total number of the same symbols shown on the screen at the end of a spin.`,
   `\n7+ symbols: symbol's amount * symbol's cost + a half of your stake\n14+ symbols: symbol's amount * double symbol's cost + a half of your stake
   \n7+ girls: girl's amount * girls's cost + a half of your stake\n14+ girls: girl's amount * double girl's cost + a half of your stake
-  \nMinimal stake: ¤ 0.20\nMaximal stake: ¤ 10`];
+  \nMinimum stake: ¤ 0.20\nMaximum stake: ¤ 10`];
