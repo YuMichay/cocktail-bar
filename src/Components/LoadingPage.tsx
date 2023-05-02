4ea579552b3cde90643b1cf4a449ca2018152fde
yuliya <julia.one.such@gmail.com>
2023-05-02 22:08:16 +0300

    docs: add project

--- src/Components/LoadingPage.tsx
+++
@@ -1,7 +0,0 @@
-import { useStore } from "../stores/store"
-
-export const LoadingPage = () => {
-  const { ImageStore } = useStore();
-
-  return <div className="loading"><img src={`${ImageStore.images.loadingImage}`} alt="loading"></img></div>
-}
